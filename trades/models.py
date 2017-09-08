c99d657dd7aa13e639dfc4381667948507cbb4e1
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2017-09-08 22:11:57 +0200

    own id field created

--- trades/models.py
+++ trades/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.crypto import get_random_string
 
 
 class TypeMoney(models.Model):
@@ -6,7 +7,8 @@ class TypeMoney(models.Model):
 
 
 class Trade(models.Model):
-    # Añadir el id personalizado
+    my_id = models.CharField(
+        max_length=9, primary_key=True, editable=False, default=create_id)
     sell_currency = models.ForeignKey(TypeMoney, on_delete=models.CASCADE)
     sell_amount = models.DecimalField(decimal_places=2)
     buy_currency = models.ForeignKey(TypeMoney, on_delete=models.CASCADE)
@@ -16,3 +18,6 @@ class Trade(models.Model):
     
     def __str__(self):
         return self.sell_currency + "-->" + self.buy_currency
+
+    def create_id(self):
+        return "PR" + get_random_string(length=7)
